175658a66b8595e951e1ac908b44129613b76941
Anukritik007 <anukritik77@gmail.com>
2020-04-11 20:03:01 +0530

    added welcome page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,16 @@
 import { Routes, RouterModule } from '@angular/router';
 import { NgModule } from '@angular/core';
 import { LoginSignupComponent } from './views/login-signup/login-signup.component';
+import { WelcomePageComponent } from './views/welcome-page/welcome-page.component';
+
 
 const appRoutes:Routes =[
                 {
                     path:'',
+                    component:WelcomePageComponent
+                },
+                {
+                    path:'login',
                     component:LoginSignupComponent
                 }
             ];
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { ReactiveFormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 import {LoginSignupComponent} from './views/login-signup/login-signup.component';
+import {WelcomePageComponent} from './views/welcome-page/welcome-page.component';
 import { ContainerComponent } from './container/container.component';
 // TODO: add views as a module
 // import {ViewsModule} from './views/views.module';
@@ -13,6 +14,7 @@ import { ContainerComponent } from './container/container.component';
 @NgModule({
   declarations: [
     LoginSignupComponent,
+    WelcomePageComponent,
     ContainerComponent
   ],
   imports: [
